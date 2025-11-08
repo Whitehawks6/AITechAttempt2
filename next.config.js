51f4e27d792d963458cd786e6b24ef7535e0a961
Whitehawks6 <plochd6@gmail.com>
2025-11-07 20:00:23 -0600

    Modify next.config.js to allow all HTTPS images
    Updated image remote patterns to allow all HTTPS images.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // ←←← ADD THIS BLOCK ↓↓↓
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
-      { protocol: 'https', hostname: '**.wikimedia.org' },
+      {
+        protocol: 'https',
+        hostname: '**', // Allows ALL https images (safe for your use case)
+      },
     ],
   },
+  // ↑↑↑ END OF FIX
 };
-module.exports = nextConfig;
 
+module.exports = nextConfig;
