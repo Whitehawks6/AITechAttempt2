5bc74e7b176379afa2cf0f2ef4d2330f77c3f206
Whitehawks6 <plochd6@gmail.com>
2025-11-07 20:30:23 -0600

    Format next.config.js for consistency

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
     remotePatterns: [
       {
@@ -14,3 +13,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
