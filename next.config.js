6eb155941a6c8b237ecac3b18da4a3f3372c9d9e
Whitehawks6 <plochd6@gmail.com>
2025-11-08 22:28:24 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
+// next.config.js - Create this file in root if missing
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',        // Allows ANY website
-        pathname: '/**',       // Allows ANY folder or file
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
       },
+      // Add more if other external domains, e.g., for logo if hosted elsewhere
     ],
   },
 };
 
 module.exports = nextConfig;
-
